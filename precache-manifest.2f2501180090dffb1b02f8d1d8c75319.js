31e1c81686cc6ad0ebf0053a682e75f9a76346b1
John Delta <john.deligiannis1@gmail.com>
2019-08-07 17:45:33 +0300

    Updates

---
+++ precache-manifest.2f2501180090dffb1b02f8d1d8c75319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c29fb335a6a21f5bd7a2be67705ca9",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "3615884385ac8ec2231c",
+    "url": "/react-calculator/static/css/main.47d6a3ed.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3615884385ac8ec2231c",
+    "url": "/react-calculator/static/js/main.ecba13cc.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
