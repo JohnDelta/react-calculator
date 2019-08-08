f235dd26f5d0b30c67fcadab4dfbda5daaf0e873
John Delta <john.deligiannis1@gmail.com>
2019-08-08 12:56:49 +0300

    mapping output text to display properly with cursor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Display from './Display.js';
 import Numpad from './Numpad.js';
 
-function App() {
-	return (
-		<div className="App">
-			<Display />
-			<Numpad />
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			displayText : "",
+			displayCursorPos : 0
+		};
+	}
+	
+	componentWillMount() {
+		this.setState({
+			displayText : "hello",
+			displayCursorPos : this.state.displayText.length-2
+		});
+	}
+	
+	render() {
+		return (
+			<div className="App">
+				<Display 
+					displayText={this.state.displayText}
+					displayCursorPos={this.state.displayCursorPos}
+				/>
+				<Numpad />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Display.js
+++ src/Display.js
@@ -3,21 +3,28 @@ import React from 'react';
 class Display extends React.Component {
 	constructor(props) {
 		super(props);
-		this.handleEvent = this.handleEvent.bind(this);
-	}
-	
-	
-	handleEvent(e) {
-		var txt = document.getElementById("text");
-		var len = txt.value.length;
-		txt.setSelectionRange(0,len-1);
 	}
 	
 	render() {
+		/*
+			In order to display the text with a cursor
+			seperator which can be moved manualy by buttons(from user),
+			(and not use the default text or textarea method)
+			i have to transform the text to an array and put a symbol
+			of cursor in the proper position each time.
+		*/
+		let text = [...this.props.displayText].map( (item,index) => {
+			return <div className="value" key={"char"+index} >{item}</div>
+		});
+		let k1 = text.slice(0,this.props.displayCursorPos);
+		let k2 = text.slice(this.props.displayCursorPos,text.length);
+		let result = [...k1,<div className="cursor" key="charSep">|</div>,...k2];
 		
 		return(
-			<div className="Display">
-				<input id="text" type="text" value="asdfsdf" onFocus={this.handleEvent}/>
+			<div 
+				className="Display"
+			>
+				{result}
 			</div>
 		);
 	}
