f5fd904d3faffa6ca478ebe0d2270cbe2b000736
John Delta <john.deligiannis1@gmail.com>
2019-08-08 22:29:31 +0300

    Updates

--- precache-manifest.077d90f3a709b6fb70e3e78bbb901567.js
+++ precache-manifest.077d90f3a709b6fb70e3e78bbb901567.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8f3b0a5597042ab2e77c0e42d317579",
+    "revision": "53a8677625301bc8fb35194bfdaa7c1a",
     "url": "/react-calculator/index.html"
   },
   {
-    "revision": "cf0d81fa29b578bfb10e",
-    "url": "/react-calculator/static/css/main.bb2e6378.chunk.css"
+    "revision": "1208d3485f8fde864f6d",
+    "url": "/react-calculator/static/css/main.eb06337b.chunk.css"
   },
   {
     "revision": "00496b9ef4500f12c49d",
     "url": "/react-calculator/static/js/2.b12bab77.chunk.js"
   },
   {
-    "revision": "cf0d81fa29b578bfb10e",
-    "url": "/react-calculator/static/js/main.225e5cbc.chunk.js"
+    "revision": "1208d3485f8fde864f6d",
+    "url": "/react-calculator/static/js/main.b3e55971.chunk.js"
   },
   {
     "revision": "e546b3e323215319a407",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.71783c5c0272d16615d19ebe0b01231d.js"
+  "/react-calculator/precache-manifest.077d90f3a709b6fb70e3e78bbb901567.js"
 );
 
 self.addEventListener('message', (event) => {
